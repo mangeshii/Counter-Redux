9f6a9f65077422d8680bab5244a8441d043b491b
mangeshipatil <thisismangeshi@gmail.com>
2022-04-07 10:35:02 +0530

    Added reducer function

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,22 @@
 import {ADD_ONE, MINUS_ONE} from './action'
+
 const initialState={
-    number: 1
+    number: 0
 }
+
+function reducers(state=initialState,action){
+    switch(action.type){
+        case ADD_ONE:{
+            console.log(action.type)
+            return state.number + 1
+        }
+        case MINUS_ONE:{
+            return state.number - 1
+        }
+        default:{
+            return state
+        }
+
+    }
+}
+export default reducers
